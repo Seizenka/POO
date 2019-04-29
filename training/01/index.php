626f69a04d9456dbff93f990bfb62c7d7e4b02fe
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-04-29 12:29:28 +0200

    training mode 12H29

--- training/01/index.php
+++ training/01/index.php
@@ -2,6 +2,8 @@
     require 'personnage.php';
 
     $korra = new Personnage(); //Création d'un nouveau personnage
+    $korra->regenerer(); //Appel de la fonction, la vie de Korra passe de 80 à 100
+
     var_dump($korra);
 
 
--- training/01/personnage.php
+++ training/01/personnage.php
@@ -1,7 +1,16 @@
 <?php
     
     class Personnage {
-        
+        public $vie = 80; //Ajout de propriété->variable pour caractériser l'objet
+        public $atk = 50;
+
+        public function crier(){
+            echo 'JE SUIS L\'AVATAR KORRA!';
+        } //Ajout de méthode->fonction pour donner vie/action à l'objet
+
+        public function regenerer(){
+            $this->vie = 100; //This permet de paramétrer directement une propriété
+        }
     }
 
 
