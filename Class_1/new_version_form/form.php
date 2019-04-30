f7fe1fd288eb0e7bfc4f54a4fa3d90710e8a172d
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-04-30 14:41:09 +0200

    suite de la nouvelle version

---
+++ Class_1/new_version_form/form.php
@@ -0,0 +1,36 @@
+<?php
+    class Form {
+
+        public function create(){
+
+        }
+
+        public function input(){
+
+        }
+
+        public function select(){
+
+        }
+
+        public function textarea(){
+
+        }
+
+        public function checkbox(){
+
+        }
+
+        public function submit(){
+
+        }
+
+        public function end(){
+            
+        }
+    }
+
+
+
+
+?>
--- Class_1/new_version_form/index.php
+++ Class_1/new_version_form/index.php
@@ -12,16 +12,29 @@
 <body>
     <main id="main">
         <h1>Nouvelle version du formlaire POO</h1>
+            <h2>BeCode</h2>
 
         <!-- On entoure le formulaire -->
         <section class="body-form">
             <?php
 
                 $form = new Form(); //Création d'un nouveau formulaire
-                echo $form->create($action); // créer le début du formulaire
+                echo $form->create(); // créer le début du formulaire
+                
+                echo $form->label('Veuillez inscrire votre nom', "name"); //Création de label pour le nom
+                echo $form->input('Veuillez inscrire votre nom',$nom); // Créer un input de type texte avec comme valeur par défaut $nom
 
-                echo $form->input('Veuillez mettre votre nom',$nom); // créer un input de type texte avec comme valeur par défaut $nom
-                echo $form->input('prenom',$prenom); //Input de type texte avec comme valeur par défaut $prenom
+                echo $form->label('Veuillez inscrire votre prénom'); //Création de label pour le prénom
+                echo $form->input('Veuillez inscrire votre prénom',$prenom); //Input de type texte avec comme valeur par défaut $prenom
+
+                echo $form->label("La ville où vous habitez", "ville"); //Création de label pour la ville
+                echo $form->select(array("Charleroi", "Bruxelles", "Namur", "Mons", "Anvers", "Liège", "ville")); //Création d'un tableau pour les villes
+
+                echo $form->label("Vous êtes :", "genre"); //Création de label pour le genre
+                echo $form->checkbox(array("Femme", "Homme")); //Création d'un tableau pour le genre
+
+                echo $form->label("Votre expérience chez BeCode", "texte"); //Création de label pour le textearea
+                echo $form->textarea("texte", "texte");
 
                 echo $form->submit('Envoyer'); //Bouton pour soumettre le formulaire se nommant "envoyer"
                 echo $form->end(); //On ferme le formulaire
