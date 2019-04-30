cb6ebc2441a712a30726a8469c5a9df38af7d6db
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-04-30 12:19:46 +0200

    push files new_version

---
+++ Class_1/new_version_form/index.php
@@ -0,0 +1,23 @@
+<?php require 'form.php' ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Nouvelle version du formulaire POO</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+</head>
+<body>
+    <main id="main">
+        <h1>Nouvelle version du formlaire POO</h1>
+
+        <section class="body-form">
+
+        </section>
+    </main>
+
+<script src="main.js"></script>
+</body>
+</html>
