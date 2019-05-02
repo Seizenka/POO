312cb23bf7de775ad3647f03ab47726e08b58062
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-05-02 09:10:13 +0200

    push files

--- Class_1/new_version_form/form.php
+++ Class_1/new_version_form/form.php
@@ -3,37 +3,50 @@
 
         public $action; //Propriétés d'un formulaire en html "action" et "method"
         public $method;
+        public $surround; // 'p'
         function __construct($action, $method){
 
             $this->action = $action; //__construct est appelé pour créer une représentation d'objet
             $this->method = $method; // Et reprendra les données du formulaire
         }
+
+        private function surround($html){
+
+            return "<{$this->surround}>$html</{$this->surround}>"; // Pour que la variable soit interprétée par les " il faut rajouter {}
+
+        }
         
 
         public function create(){
 
-            return '<form action="' . $this->action . '" method="' . $this->method . '">';
+            echo '<form action="' . $this->action . '" method="' . $this->method . '">';
 
         } //fonction create pour le début de formulaire
 
         public function label($for){
             
-            return '<label for="' . $for . '">Veuillez inscrire votre nom</label>';
+            echo $this->surround('<label for="' . $for . '">Veuillez inscrire votre nom</label>');
 
         } //fonction label pour le titre des champs
 
         public function input($type, $id, $name, $value){
 
-            return 'input type="' . $type . '" id="' . $id . '" value="' . $value . '" name="' . $name . '">';
-            
+            echo $this->surround('<input type="' . $type . '" id="' . $id . '" value="' . $value . '" name="' . $name . '">');
+
         } //fontion input pour les champs de formulaire, on appelle les attributs d'un input
 
-        public function select(){
+        public function select($id, $option){
+            echo $this->surround('<select id="' . $id .'">');
 
-        }
+            foreach($option as $option){
+                echo '<option value="' . $option . '">'$option'</option>';
+            }
+
+            echo $this->surround('</select>');
+        } //fonction select pour la sélection du genre et création d'une boucle foreach pour chaque élément
 
         public function textarea(){
-
+            echo '<textarea></textarea>';
         }
 
         public function checkbox(){
